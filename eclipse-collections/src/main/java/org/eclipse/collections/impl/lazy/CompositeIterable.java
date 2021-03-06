482c6edd9c060dbf87a36f29f00caf6bf52c649f
Mostafa Jangali <massi@Mostafas-Mac-mini.local>
2021-03-06 08:35:05 -0500

    pmt-hwo2

--- eclipse-collections/src/main/java/org/eclipse/collections/impl/lazy/CompositeIterable.java
+++ eclipse-collections/src/main/java/org/eclipse/collections/impl/lazy/CompositeIterable.java
@@ -59,6 +59,13 @@ public final class CompositeIterable<E>
             objectIntProcedure.value(object, index.getCount());
             index.increment();
         }));
+    	try {
+    		org.eclipse.collections.impl.myBlackhole.get_value("1");
+			Thread.sleep(0,100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @Override
